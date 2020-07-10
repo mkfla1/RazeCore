8094538c7eb44c7180b74160148b0368264b5cf1
MKFLA <mkfla.work@gmail.com>
2020-07-10 17:26:08 +0800

    colorFromHexString

--- RazeCore/Tests/RazeCoreTests/RazeCoreTests.swift
+++ RazeCore/Tests/RazeCoreTests/RazeCoreTests.swift
@@ -2,16 +2,12 @@ import XCTest
 @testable import RazeCore
 
 final class RazeCoreTests: XCTestCase {
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct
-    // results.
-    XCTAssertEqual(RazeCore().text, "Hello, World!")
+  func testColorRedEqual() {
+    let color = RazeCore.colorFromHexString("FF0000")
+    XCTAssertEqual(color, .red)
   }
   
-  
-  
   static var allTests = [
-    ("testExample", testExample),
+    ("testColorRedEqual", testColorRedEqual),
   ]
 }
