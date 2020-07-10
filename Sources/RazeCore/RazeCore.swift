430f49ae3f7cc0e97e9fbee5e48051c75466a3ad
MKFLA <mkfla.work@gmail.com>
2020-07-11 00:56:51 +0800

    Added secondary color as static variable.

--- Sources/RazeCore/RazeCore.swift
+++ Sources/RazeCore/RazeCore.swift
@@ -29,4 +29,9 @@ public class RazeCore {
   public static var razeColor: UIColor {
     return self.colorFromHexString("006736")
   }
+  
+  /// Returns an object of 'UIColor' with the second most eye-pleasing color known to all humanity
+  public static var secondaryRazeColor: UIColor {
+    return colorFromHexString("FCFFFD")
+  }
 }
