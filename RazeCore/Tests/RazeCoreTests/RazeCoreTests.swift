24088e80e1ef5152bc3cb52064b27976d12f6194
MKFLA <mkfla.work@gmail.com>
2020-07-10 16:20:01 +0800

    adjust indent

--- RazeCore/Tests/RazeCoreTests/RazeCoreTests.swift
+++ RazeCore/Tests/RazeCoreTests/RazeCoreTests.swift
@@ -2,14 +2,16 @@ import XCTest
 @testable import RazeCore
 
 final class RazeCoreTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(RazeCore().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    XCTAssertEqual(RazeCore().text, "Hello, World!")
+  }
+  
+  
+  
+  static var allTests = [
+    ("testExample", testExample),
+  ]
 }
--- RazeCore/Tests/RazeCoreTests/XCTestManifests.swift
+++ RazeCore/Tests/RazeCoreTests/XCTestManifests.swift
@@ -2,8 +2,8 @@ import XCTest
 
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(RazeCoreTests.allTests),
-    ]
+  return [
+    testCase(RazeCoreTests.allTests),
+  ]
 }
 #endif
